12895871f32f7b22726141e7165378d725fcd91a
Maksim <shumskiydmitry76@mail.ru>
2025-04-16 19:47:02 +0300

    create window

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,27 @@
 #include <SFML/Graphics.hpp>
-#include <SFML/Window.hpp>
-#include <memory>
 
-#include "Game.h"
+using namespace sf;
+
+const size_t SCREEN_W = 1920;
+const size_t SCREEN_H = 1080;
 
 int main() 
 {
-    auto game = std::make_unique<Game>();
-    game->run();
-    return 0;
+    RenderWindow window({ SCREEN_W, SCREEN_H }, "SloteGame", Style::Close | Style::Titlebar);
+
+    while (window.isOpen())
+    {
+        Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        window.clear();
+
+        window.display();
+    }
 }
